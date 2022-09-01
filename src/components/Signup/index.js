18a1bb426af14a40eb78e741604ff305498628b0
sgujuluva <sanghee04@gmail.com>
2022-09-01 09:36:17 +0200

    handlechange created for form

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -4,25 +4,29 @@ import "./Signup.css"
 
 function SignUp() {
     const [user,setUser] = useState({username:"", email:"", password:""})
+
     const handleChange = (e) => {
         setUser(prev => ({
             ...prev,
-            [e]:e.target.value //[] => dynamic way of key in obj
+            [e.target.name]:e.target.value //[] => dynamic way of key in obj
         }))
     }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
   return <div>
-    <form>
+    <form onSubmit={handleSubmit}>
         <div className="username">
         <span>Username:</span>
-       <input onChange={handleChange} type="text" placeholder="Enter your username" required />
+       <input onChange={handleChange} type="text" placeholder="Enter your username" name = "username" required />
        </div>
        <div className="email">
        <span>Email:</span>
-       <input type="email" placeholder="Enter your Email" required />
+       <input onChange={handleChange} type="email" placeholder="Enter your Email" name = "email" required />
        </div>
        <div className="password">
        <span>Password:</span>
-       <input type="password" placeholder="Enter your password"  required/>
+       <input onChange={handleChange} type="password" placeholder="Enter your password" name = "password" required/>
        </div>
        <button>Sign Up</button>
     </form>
