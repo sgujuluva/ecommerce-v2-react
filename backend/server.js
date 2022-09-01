9f3a6dbb0eff805902576fce949ddbde5ba54acb
sgujuluva <sanghee04@gmail.com>
2022-09-01 11:49:32 +0200

    checking email

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
-import 
+import express from "express";
+import "dotenv/config";
+import { Low, JSONFile } from 'lowdb'
+
+const app = express();
+//lowdb
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+//read data from json
+await db.read();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running at the PORT 5000");
+})
+
+const uid = () => {
+    return Date.now().toString(16)
+}
+
+app.post("/api/signup", async(req, res) => {
+    if (db.data.users.some(item => item.email !== req.body.email)){
+        db.data.users.push(req.body);
+        await db.write();
+        res.send({user: req.body, msg : "user added to db", success : true})
+    }else {
+        res.send({msg:"user already exist"})
+    }
+   
+})
