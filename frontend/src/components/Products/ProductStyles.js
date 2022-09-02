c8dd9f74e644ad86d977cf09c71637e2920c4db8
sgujuluva <sanghee04@gmail.com>
2022-09-02 12:43:11 +0200

    styling prods

--- frontend/src/components/Products/ProductStyles.js
+++ frontend/src/components/Products/ProductStyles.js
@@ -1,31 +1,36 @@
 import styled from "styled-components";
 export const WrapperProd = styled.div`
 display:flex;
-justify-content:space-evenly;
+justify-content:center;
 flex-direction:column;
-width:100%;
-border:2px solid grey;
+box-shadow: 0 0 10px;
 border-radius:25px;
-height:100%;
+padding:2%;
 
 button{
     border-radius:20px;
     background-color:orange;
     width:30%;
     font-weight:800;
+    margin-left:40%;
 }
 img{
     width:100%;
-max-height:450px;
+max-height:300px;
+margin:3% 0;
 object-fit:contain;
 border-radius:25px;
 }
-div{
-    padding:1%;
-    height:100%;
+.prod-details{
+    width: 95%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     h3{
         font-size:1.3rem;
-    }
+        }
     p{
         line-height:1.5rem;
     }
