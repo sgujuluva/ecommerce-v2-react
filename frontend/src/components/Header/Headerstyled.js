f51eb5b435c8aec02a24753c5e1c5c5ed04f1e6d
sgujuluva <sanghee04@gmail.com>
2022-09-02 17:55:02 +0200

    styling header

---
+++ frontend/src/components/Header/Headerstyled.js
@@ -0,0 +1,3 @@
+import Styled from "styled-components"
+
+export const Wrapper = Styled.div``;
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
 import "./Header.css"
+import {Wrapper} from "./Headerstyled"
 
 function Header() {
   return (
     <div className="header">
 <NavLink to={"/"}>Home</NavLink>
+<div className="search-item">
+<div className="search">
+<input type="text" placeholder="Search item" />
+</div>
+<div className="search-button">
+  <button>Search</button>
+</div>
+</div>
 <NavLink to={"/signup"}>Sign in</NavLink>
 <NavLink to={"/login"}>Login</NavLink>
     </div>
