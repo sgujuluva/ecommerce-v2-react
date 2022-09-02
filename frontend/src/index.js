7eec95f4360eb554540fb7357d538e3a9d92f137
sgujuluva <sanghee04@gmail.com>
2022-09-02 10:19:52 +0200

    queryclient provider wrapped in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,17 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "react-query"
 
-
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={client}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
