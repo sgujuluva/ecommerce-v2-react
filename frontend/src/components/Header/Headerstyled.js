5e43c96a6645627d54e00bb9271d3a849f46d60f
sgujuluva <sanghee04@gmail.com>
2022-09-02 19:40:34 +0200

    styled navbar

--- frontend/src/components/Header/Headerstyled.js
+++ frontend/src/components/Header/Headerstyled.js
@@ -2,10 +2,34 @@ import Styled from "styled-components"
 
 export const Wrapper = Styled.div`
 background-color:orange;
+padding:1%;
 display:flex;
 justify-content : space-evenly;
 align-items:center;
 a{
-    border:2px solid red;
+    text-decoration:none;
+    color:black;
+    font-size:1.5rem;
+    font-weight:800;
+}
+.search-item{
+  
+    width:25%;
+    display:flex;
+    justify-content:space-evenly;
+    align-items:center;
+ }
+.search, .search-button{
+    padding:2%;
+    font-weight:800;
+}
+.search input{
+      padding:4%;
+      border-radius:25px;
+
+}
+.search-button button{
+   padding:4%;
+   border-radius:25px;
 }
 `;
