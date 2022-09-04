7e915cabb3d1e58dd09a8c85de809d48e4df404c
sgujuluva <sanghee04@gmail.com>
2022-09-04 10:02:29 +0200

    image fixed to responsive

--- frontend/src/components/Products/ProductStyles.js
+++ frontend/src/components/Products/ProductStyles.js
@@ -1,45 +1,45 @@
 import styled from "styled-components";
 export const WrapperProd = styled.div`
-display:flex;
-justify-content:center;
-flex-direction:column;
-box-shadow: 0 0 10px;
-border-radius:25px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  box-shadow: 0 0 10px;
+  border-radius: 25px;
 
+  button {
+    border-radius: 20px;
+    background-color: orange;
+    font-weight: 800;
+  }
+  img {
+    width: 80%;
+    height: 20vh;
 
-button{
-    border-radius:20px;
-    background-color:orange;
-       font-weight:800;
-}
-img{
-  width:250px;
-  height:250px;
-    margin-left:10%;
-border-radius:25px;
-}
-.prod-details{
+    object-fit: contain;
+    margin-left: 10%;
+    border-radius: 25px;
+  }
+  .prod-details {
     width: 95%;
-    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    h3{
-        font-size:1.3rem;
-        display: -webkit-box;
-        -webkit-line-clamp: 1;
-        -webkit-box-orient: vertical; 
-        overflow: hidden; 
-        }
-    p{
-        line-height:1.5rem;
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical; 
-        overflow: hidden; 
+    h3 {
+      font-size: 1.3rem;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
     }
-    
-}
+    p {
+      width: 100%;
 
+      line-height: 1.5rem;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+  }
 `;
