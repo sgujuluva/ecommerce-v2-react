6ba4a1d9061edc229e2329c46f734817dda06f21
sgujuluva <sanghee04@gmail.com>
2022-09-06 12:19:32 +0200

    updating the quantity item

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -12,6 +12,7 @@ function Cart({item}) {
         <div className="cart-container">
         <h5>Cart({totalUniqueItems}) total Items: ({totalItems})</h5>
         <table>
+            <tbody>
             {items.map((item,index) => {
                 return (
                <tr key={index}>
@@ -22,11 +23,14 @@ function Cart({item}) {
                     <td>{item.price}</td>
                     <td>Quantitty ({item.Quantity})</td>
                     <td>
-                        
+                        <button className="btn btn-info ms-2" onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
+                        <button className="btn btn-info ms-2">-</button>
+                        <button className="btn btn-danger ms-2">Remove Item</button>
                     </td>
                </tr> 
                 )
             })}
+            </tbody>
         </table>
         </div>
     
