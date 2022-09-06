d335c10fc60e15863ee886643995446eb96d348c
sgujuluva <sanghee04@gmail.com>
2022-09-06 12:24:45 +0200

    removed item

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -21,7 +21,7 @@ function Cart({item}) {
                     </td>
                     <td>{item.title}</td>
                     <td>{item.price}</td>
-                    <td>Quantitty ({item.Quantity})</td>
+                    <td>Quantity ({item.Quantity})</td>
                     <td>
                         <button className="btn btn-info ms-2" 
                                 onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>
@@ -29,7 +29,9 @@ function Cart({item}) {
                         <button className="btn btn-info ms-2"
                                 onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>
                             -</button>
-                        <button className="btn btn-danger ms-2">Remove Item</button>
+                        <button className="btn btn-danger ms-2"
+                                onClick={() => removeItem(item.id)}
+                        >Remove Item</button>
                     </td>
                </tr> 
                 )
