b8984f369a4494389288efc4ef7f034bf90ae8fe
sgujuluva <sanghee04@gmail.com>
2022-09-06 12:20:24 +0200

    updating the quantity item

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -23,8 +23,12 @@ function Cart({item}) {
                     <td>{item.price}</td>
                     <td>Quantitty ({item.Quantity})</td>
                     <td>
-                        <button className="btn btn-info ms-2" onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>+</button>
-                        <button className="btn btn-info ms-2">-</button>
+                        <button className="btn btn-info ms-2" 
+                                onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>
+                                    + </button>
+                        <button className="btn btn-info ms-2"
+                                onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>
+                            -</button>
                         <button className="btn btn-danger ms-2">Remove Item</button>
                     </td>
                </tr> 
