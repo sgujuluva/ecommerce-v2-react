b8fd1bd5df1c88d5906f3548d856253b1dd37ab5
sgujuluva <sanghee04@gmail.com>
2022-09-06 13:15:15 +0200

    clear cart functioanlity added

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -1,49 +1,77 @@
 import React from "react";
 import { useCart } from "react-use-cart";
 
-function Cart({item}) {    
+function Cart({ item }) {
+  const {
+    isEmpty,
+    totalUniqueItems,
+    totalItems,
+    items,
+    updateItemQuantity,
+    removeItem,
+    cartTotal,
+    emptyCart,
+  } = useCart();
 
-  const { isEmpty, totalUniqueItems,totalItems, items, updateItemQuantity, removeItem,cartTotal } = useCart();
-
-    if(isEmpty) return <p>Your Cart is Empty</p>
+  if (isEmpty) return <p>Your Cart is Empty</p>;
 
   return (
     <section>
-        <div className="cart-container">
-        <h5>Cart({totalUniqueItems}) total Items: ({totalItems})</h5>
+      <div className="cart-container">
+        <h5>
+          Cart({totalUniqueItems}) total Items: ({totalItems})
+        </h5>
         <table>
-            <tbody>
-            {items.map((item,index) => {
-                return (
-               <tr key={index}>
-                    <td>
-                        <img src={item.image} style={{height:"6rem"}} alt="" />
-                    </td>
-                    <td>{item.title}</td>
-                    <td>{item.price}</td>
-                    <td>Quantity ({item.Quantity})</td>
-                    <td>
-                        <button className="btn btn-info ms-2" 
-                                onClick={() => updateItemQuantity(item.id, item.quantity + 1)}>
-                                    + </button>
-                        <button className="btn btn-info ms-2"
-                                onClick={() => updateItemQuantity(item.id, item.quantity - 1)}>
-                            -</button>
-                        <button className="btn btn-danger ms-2"
-                                onClick={() => removeItem(item.id)}
-                        >Remove Item</button>
-                    </td>
-               </tr> 
-                )
+          <tbody>
+            {items.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    <img src={item.image} style={{ height: "6rem" }} alt="" />
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.price}</td>
+                  <td>Quantity ({item.Quantity})</td>
+                  <td>
+                    <button
+                      className="btn btn-info ms-2"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity + 1)
+                      }
+                    >
+                      +{" "}
+                    </button>
+                    <button
+                      className="btn btn-info ms-2"
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantity - 1)
+                      }
+                    >
+                      -
+                    </button>
+                    <button
+                      className="btn btn-danger ms-2"
+                      onClick={() => removeItem(item.id)}
+                    >
+                      Remove Item
+                    </button>
+                  </td>
+                </tr>
+              );
             })}
-            </tbody>
+          </tbody>
         </table>
-        </div>
-    <div className="col-auto ms-auto">
-<h2>Total Price : {cartTotal}</h2>
-    </div>
-  </section>
-  )
+      </div>
+      <div className="col-auto ms-auto">
+        <h2>Total Price : {cartTotal}</h2>
+      </div>
+      <div className="col-auto ms-auto">
+        <button className="btn btn-danger m-2" 
+                onClick={() => emptyCart() }
+        >Clear Cart</button>
+      </div>
+    </section>
+  );
 }
 
 export default Cart;
