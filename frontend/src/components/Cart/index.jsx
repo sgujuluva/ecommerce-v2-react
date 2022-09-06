eded118aa1ed4da38fdaabac9febbc88716f1639
sgujuluva <sanghee04@gmail.com>
2022-09-06 12:15:24 +0200

    creating table structure for cart component

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useCart } from "react-use-cart";
 
-function Cart({item}) {
-
-    const{prod} = useCart();
+function Cart({item}) {    
 
   const { isEmpty, totalUniqueItems,totalItems, items, updateItemQuantity, removeItem } = useCart();
 
@@ -13,6 +11,23 @@ function Cart({item}) {
     <section>
         <div className="cart-container">
         <h5>Cart({totalUniqueItems}) total Items: ({totalItems})</h5>
+        <table>
+            {items.map((item,index) => {
+                return (
+               <tr key={index}>
+                    <td>
+                        <img src={item.image} style={{height:"6rem"}} alt="" />
+                    </td>
+                    <td>{item.title}</td>
+                    <td>{item.price}</td>
+                    <td>Quantitty ({item.Quantity})</td>
+                    <td>
+                        
+                    </td>
+               </tr> 
+                )
+            })}
+        </table>
         </div>
     
   </section>
